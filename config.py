2aff1e4b1f98715531763fc9bb11d712d0b0ed07
IrlanBarros <irlanbarros100@gmail.com>
2026-05-10 19:52:23 -0300

    feat: add configuration file with game settings and rules

---
+++ config.py
@@ -0,0 +1,32 @@
+# config.py
+WIDTH, HEIGHT = 1280, 720
+
+# Paleta Moderna
+BG_COLOR = (241, 245, 249)      
+TEXT_COLOR = (30, 41, 59)       
+RED = (239, 68, 68)             
+GREEN = (34, 197, 94)           
+BLUE = (59, 130, 246)           
+WHITE = (255, 255, 255)
+BLACK = (15, 23, 42)
+SHADOW = (203, 213, 225)     
+
+number_rounds = 5
+
+RULES = [
+    "Bem-vindo ao Probabilidado!",
+    "Cada jogador começa com 15 fichas.",
+    "A cada rodada dois dados secretos (não viciados) são lançados.",
+    "O número das faces que saírem nos dados são usados como parâmetro",
+    "para o par ordenado, exemplo:",
+    "Dado 1: 5; dado 2: 4. O par ordenado então é (5, 4).",
+    "Você pode apostar em quadrantes, cores ou par ordenado.",
+    "Quadrantes: Aposte no quadrante onde os dados cairão.",
+    "Cores: Aposte na cor da célula selecionada.",
+    "Apostar: não clique em nada! Digite a quantiddade de fichas e aperte enter.",
+    "Para apostar em quadrante ou cor, aposte nos botões da lateral.",
+    "Par ordenado: Aposte nas coordenadas exatas dos dados.",
+    "Os ganhos variam conforme a probabilidade do evento e o",
+    "numero de fichas apostadas.",
+    "Pressione ENTER para continuar."
+]
